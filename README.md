5d94c07ef32f41a63237d775fd614aa420ae7c1d
Jordan Safford and Vera Weikel <vera@>
2019-08-28 16:38:21 -0700

    Add changed for the logic; does not function properly.

---
+++ README.md
@@ -0,0 +1,149 @@
+# Places Visited
+
+#### _Hired to build a Places Visited app
+
+#### _Project authored by Jordan Safford and Vera Weikel_
+
+## Table of contents
+* [Description](#description)
+* [Setup/Installation Requirements](#setup-installation-requirements)
+* [Github Repository](#github-repository)
+* [Known Bugs](#known-bugs)
+* [Technologies Used](#technologies-used)
+* [Support](#support)
+* [License](#license)
+
+
+### Description
+_A project to create and build a places (visited) app according to specs:_
+
+Create a web application that takes an input and writes, updates, deletes to the app.
+
+
+### App should:
+
+- Spec: Accept user inputted text
+Input: "text"
+Output: "text"
+- Spec: The program displays inputted text
+Input: "text"
+Output: "text"
+- Spec: Each destination should be an object with multiple properties(ie. like location, landmarks, time of year, notes, etc.)
+- Spec: Input text to display in a "Places" List one line at a time
+Input: "input a"|input b"
+Output: "input a"| "input b"
+- Spec: Display those properties when a user clicks on a place's name.
+Input: "Done" (option chosen)
+Output: "Stricken LineItem" 'Done'.
+
+
+Behavior
+Input
+Output
+Roll a dice
+1
+2
+3
+4
+5
+6
+1
+2
+3
+4
+5
+6
+Reset roll if a “1” is rolled
+1
+0
+Add roll total to score
+Total + current score
+Score total
+Hold
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Use Case:
+A user should be able to enter a new to do list and see new results over and over again.
+
+- Example: If a user inputs a to do list, the program should display a list of values:
+<br/>
+Input: "input a"|input b"
+Output: "input a"| "input b"
+
+
+## Setup/Installation Requirements
+<pre>
+1. [**Github:**The Places Visited**](https://github.com/QuietEvolver/the-places-visited.git)
+2. Open the command line--and navigate into the repository.
+3. On your browser, open the desired file, and enjoy!
+</pre>
+
+## Known Bugs
+<pre>
+* Repositories are subject to update in the Dev and/or Production process.
+</pre>
+
+## Technologies Used
+<pre>
+* Atom
+* Markdown
+* CSS
+* GitHub
+* HTML
+* Bootstrap
+* Command Line Interface
+* JavaScript
+* jQuery
+</pre>
+
+## Support
+<pre>
+quietevolver@github.com
+jordansafford@gmail.com
+</pre>
+_Please contact with questions and/or comments._
+
+### License
+
+* This software is licensed under MIT
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 //be
-function PigDice() {
-  this.name = name,
-  this.diceTotal = total
 
+//PigDice is the game Constructor
+function PigDice(name, diceTotal, scoreTotal, turnTotal) {
+  this.name = name,
+  this.diceTotal = diceTotal,
+  this.scoreTotal = scoreTotal,
+  this.turnTotal = turnTotal
 }
 
-// var playerOne = new PigDice(name, diceTotal)
-// var playerTwo = new PigDice(name, diceTotal)
-var playerOne = new PigDice(playerOneName, playerOneDiceTotal);
-var playerTwo = new PigDice(playerTwoName, playerTwoDiceTotal);
+//user declaration
+var playerOne = new PigDice();
+var playerTwo = new PigDice();
+
+var currentTurn;
 
 PigDice.prototype.diceRoll = function () {
   Math.floor(Math.random() * 6) + 1;
@@ -17,10 +21,54 @@ PigDice.prototype.diceRoll = function () {
 //fe
 $(document).ready(function() {
   console.log("doc ready fxn?");
-  $("#name-button").submit(function(event) {
+  $("form").submit(function(event) {
     console.log("in name button fxn?");
-    var name = $("input#name").val();
-    console.log("name", name);
+    var name = $("#name").val();
+    console.log("Your name is: ", name);
+    event.preventDefault();
     });
-  event.preventDefault();
+
+  // Get random dice number
+function throwDice () {
+  var diceRoll = Math.floor( Math.random() * 6 ) +1;
+  console.log(diceRoll);
+  User.isOne(); // Need to be updated
+}
+
+// User Interface logic
+function clickHold() {
+    $("input#throw-total1").val("");
+    $("input#throw-total2").val("");
+    this.scoreTotal += this.turnTotal;
+    $("input#score1").text(this.scoreTotal);
+    $("input#score2").text(this.scoreTotal);
+}
+
+$(document).ready(function(){
+
+  $("button#hold").on("click", function(event) {
+    event.preventDefault();
+    clickHold();
+  });
+});
+// function attachListeners() {
+//
+// var rollButton =
+//   $("form#roll-button").submit(function(event) {
+//     console.log("in name roll-button fxn?");
+//     var rollButton = parseInt($("#roll-button").val());
+//     console.log("Your roll-button is: ", diceRoll(), rollButton);
+//     event.preventDefault();
+//     });
+// var holdButton =
+//   $("form#holdbutton").submit(function(event) {
+//     console.log("in name hold-button fxn?");
+//     var holdButton = $("#hold-button").val();
+//     console.log("Your hold-button is: ", holdButton);
+//     event.preventDefault();
+//     });
+//   };
+  // var user = playerOne.diceRoll();
+  // console.log("playerOne diceRoll", playerOne, user)
+  // attachListeners();
 });
